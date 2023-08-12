4c38bd15d640e1f560fec49745f64b1e1ac17baf
Ajay S <ajaycshan@gmail.com>
2023-08-12 12:31:47 +1000

    Add JS to code

---
+++ prework-study-guide/assets/script.js
@@ -0,0 +1,25 @@
+// In JS, we declare a variable using var. After var keyworkd, we give the variable a name that's unique
+// === means strict equality to see if 2 values are equal and returns a boolean in result
+
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (var x = 0; x < topics.length; x++){
+
+    console.log(topics[x]);
+
+}
+
+
+/*
+if (topic === 'HTML') {
+  console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+  console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+  console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log('Please try again!');
+}
+*/
